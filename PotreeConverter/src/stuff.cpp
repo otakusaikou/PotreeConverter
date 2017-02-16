7b85e9dff442b552095376fa128bf14edfa7bc57
otakusaikou <otakuzyoutou@gmail.com>
2017-02-17 07:39:23 +0900

    [Change] Remove OS check, only compile for LINUX

--- PotreeConverter/src/stuff.cpp
+++ PotreeConverter/src/stuff.cpp
@@ -34,15 +34,7 @@ using std::binary_function;
 using std::map;
 
 
-#ifdef BOOST_OS_WINDOWS
-#include <Windows.h>>
-#elif BOOST_OS_LINUX
 
-#elif BOOST_OS_MACOS
-
-#elif BOOST_OS_BSD
-
-#endif
 
 
 
@@ -211,14 +203,7 @@ float psign(float value){
 string getExecutablePath(){
 
 	string path = "./";
-	
-#ifdef BOOST_OS_WINDOWS
-	char  buffer[MAX_PATH]; 
-	GetModuleFileName( NULL, buffer, MAX_PATH );
 
-	string::size_type pos = string( buffer ).find_last_of( "\\/" );
-	path = string( buffer ).substr( 0, pos);
-#elif BOOST_OS_LINUX
 	// http://stackoverflow.com/questions/5525668/how-to-implement-readlink-to-find-the-path
 	char buff[PATH_MAX];
     ssize_t len = ::readlink("/proc/self/exe", buff, sizeof(buff)-1);
@@ -234,12 +219,6 @@ string getExecutablePath(){
 	// http://stackoverflow.com/questions/799679/programatically-retrieving-the-absolute-path-of-an-os-x-command-line-app
 //#elif BOOST_OS_BSD
 	// TODO
-#else
-	cout << "WARNING: Potree was unable to identify the operating system and as a result, the directory of the executable." << endl;
-	cout << "Using current work dir as executable directory. Make sure to run potree inside the directory with the executable to avoid problems." << endl;
-	path = "./";
-#endif
-
 	return path;
 }
 
